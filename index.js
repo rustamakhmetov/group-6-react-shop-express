12fb0ffbec45a1b12f61feee20e1f11a9438b931
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2019-05-09 17:22:13 +0500

    [task2] add Price

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Image from '~src/components/Image';
 import products from '~src/constants/Products';
+
+import Image from '~src/components/Image';
 import TextBox from "./src/components/TextBox";
+import Price from "./src/components/Price";
 
 const product = products[0];
 
 ReactDOM.render(
-  <TextBox>
-    Hello, world!
-  </TextBox>,
+  <Price value={10} />,
   document.getElementById('root')
 );
---
+++ src/components/Price.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Price extends Component {
+  render() {
+    return (
+      <span>Price: {this.props.value}</span>
+    )
+  }
+}
+
+export default Price;
